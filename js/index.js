51c49d91bbc82663ec64cdbc4dd705dc0056a517
Piers Powlesland <piers.powlesland@yoti.com>
2019-03-18 15:13:29 +0000

    Extracted roller into its own file
    It's not working quite right any more, needs some investigation.

--- js/index.js
+++ js/index.js
@@ -1,9 +1,8 @@
-//import { Roller } from './roller.js';
-//import * as rol from './roller.js';
-/*jshint esversion: 6 */
-/* jshint -W097 */ // Disable warning for using strict on file.
-/*jshint browser: true */ // Disable warning of document undefined.
+
 'use strict';
+
+//import { Roller } from './roller.js';
+import * as rol from './roller.js';
 
 // Scene
 let scene = document.createElement("div");
@@ -85,54 +84,35 @@ originY.input.onchange = originY.input.oninput = updatePerspectiveOrigin;
 updatePerspectiveOrigin(); //Ensure values initialised
 
 
-/* 
-The roller is formed from a collection of segments, each segment is rotated
-around the X axis by an amount inversely proportional to the total number of
-segments, and then each segment is Z translated such that the edges of these
-segments or are aligned and just touching this means the height of the roller
-will be double the Z translation of each element. Given that we know the
-required height of the roller, we know that the Z translation will be half the
-roller height. We need to calculate the segment height such that the translation
-required to line up the edges will be equal to half the roller height. We can
-find the angle in between each segment and we know the opposite side which is
-half the roller height so we just need to calculate the adjacent which will be
-equal to half the segment height.
- */
-let rollerWidth = 250;
-let rollerHeight = 400;
-let numImgs = 5;
-let segsPerImg = 4;
-let numSegs = numImgs * segsPerImg;
+let defaultViewBox = new rol.ViewBox(1499, 500, 2, 2000);
+let svgs = [
+  new rol.SVG("https://img.cryptokitties.co/0x06012c8cf97bead5deae237070f9587f8e7a266d/1452684.svg", defaultViewBox),
+  new rol.SVG("https://img.cryptokitties.co/0x06012c8cf97bead5deae237070f9587f8e7a266d/1227462.svg", defaultViewBox),
+  new rol.SVG("https://img.cryptokitties.co/0x06012c8cf97bead5deae237070f9587f8e7a266d/1356409.svg", defaultViewBox),
+  new rol.SVG("https://img.cryptokitties.co/0x06012c8cf97bead5deae237070f9587f8e7a266d/1461111.svg", defaultViewBox),
+  new rol.SVG("https://img.cryptokitties.co/0x06012c8cf97bead5deae237070f9587f8e7a266d/1467855.svg", defaultViewBox),
+];
 
-let zTranslate = rollerHeight / 2;
+let roller = rol.CreateRoller(250, 450, svgs, 5);
 
-// The angle from the edge of a segment to the centre of the roller. Angle
-// inside a regular polygon is (n-2) × PI / n where n is the number of sides. We
-// divide by a further 2 since that angle is shared by 2 adjacent segments.
-let angle = (numSegs - 2) * Math.PI / 2 / numSegs;
+scene.appendChild(roller);
 
-// The height of each segment. 
-let segHeight = 2 * zTranslate / Math.tan(angle);
-//console.log("segHeight", segHeight);
+// animate roller
+let style = document.createElement('style');
+style.type = 'text/css';
+style.innerHTML = `@keyframes spin {
+  0% { transform: rotateX(0deg); }
+100% { transform: rotateX(720deg); }
+}`;
+document.getElementsByTagName('head')[0].appendChild(style);
 
-// Helps reduce gaps in rendering to just bridge the gap between components
-// but also borks the images a bit when more segs per image are used.
-// could look into overlapping segments, sounds promising.
-segHeight = Math.trunc(segHeight) + 1;
-
-let roller = document.createElement("div");
+let animating = false;
+scene.onclick = () => {
+  roller.style.animation = animating ? '' : 'spin 4s infinite';
+  animating = !animating;
+};
 
 
-// @ts-ignore
-roller.style = `
-    width: ${rollerWidth}px;
-    height: ${rollerHeight}px;
-    position: relative;
-    transform-origin: 50% 50%;
-    transform-style: preserve-3d;
-    border: 1px solid red;
-    margin:auto;
-`;
 
 // rotate
 let rotateY = addRangeControl("Rotate y", 0, 360, 0);
@@ -151,101 +131,6 @@ function updateTransform() {
 rotateY.input.onchange = rotateY.input.oninput = updateTransform;
 rotateZ.input.onchange = rotateZ.input.oninput = updateTransform;
 updateTransform(); //Ensure value initialised
-
-scene.appendChild(roller);
-
-let imSrc = "https://img.cryptokitties.co/0x06012c8cf97bead5deae237070f9587f8e7a266d/1089759.svg";
-
-
-/*
-The default approach taken by fragment identifiers to mapping the viewbox to
-the viewport(the img tag) is that the viewbox center will be aligned with the
-viewport center, and it will be scaled maintaining its aspect ratio such that
-it fills the viewport as far as it can without extending any edges past the
-viewport. At that point any edges not at the extent of the viewport will be
-extended to touch the viewport edges. This means if we have defined a square
-viewbox that we put in a rectangular viewport we will see a rectangular
-section of the svg.
-
-The original kitty viewbox is (0,0,3000,3000) with quite a lot of empty space
-around the kitty so we want to cut about 500 off each edge. We also want to
-ensure that each image slice fills the viewport fully vertically, this ensures
-that segments will align. We are not bothered about the horizontal aspect of our
-viewbox as long as it is centered. By setting the width to be very small and
-setting the left offset to the center of the image we ensure that the viewport
-is fully vertically filled and leave it up to the SVG system to extend the
-viewbox to the horizontal segment edges.
-*/
-
-// Original kitty viewbox (0,0,3000,3000)
-let wholeImgViewBox = {
-  height: 2000, // Shave 1000 off total height
-  top: 500,     // Offset vertically so viewbox is centered  
-  width: 2,     // Tiny width, allow svg system to extend as needed
-  left: 1499,   // Center our viewbox horizontally
-};
-
-let imgSegHeight = wholeImgViewBox.height / segsPerImg;
-
-
-
-for (let i = 0; i < numSegs; i++) {
-
-  let seg = document.createElement("img");
-  seg.setAttribute("width", String(rollerWidth));
-  seg.setAttribute("height", String(segHeight));
-
-  let imgOffset = (i * imgSegHeight) % wholeImgViewBox.height;
-
-  let svgView = imSrc +
-    `#svgView(viewBox(${wholeImgViewBox.left}, 
-    ${wholeImgViewBox.top + imgOffset}, 
-    ${wholeImgViewBox.width}, 
-    ${imgSegHeight}))`;
-
-  seg.setAttribute("src", svgView);
-
-  // @ts-ignore
-  seg.style = `
-    position: absolute;
-    background: grey;
-
-    /* ensure transforms happen around the center of the segment */
-    transform-origin: 50% 50%;
-
-    /*
-    position the segment in the center of it's parent, so push
-    it half the height of its parent down and then pull it
-    up by half of its height
-    */
-    top: 50%;
-    margin-top: ${-segHeight / 2}px;
-
-   /* border: 1px solid red;*/
-`;
-
-  seg.style.transform = `rotateX(-${360 * i / numSegs}deg) translateZ(${zTranslate}px)`;
-
-  roller.appendChild(seg);
-
-}
-
-// animate roller
-let style = document.createElement('style');
-style.type = 'text/css';
-style.innerHTML = `@keyframes spin {
-  0% { transform: rotateX(0deg); }
-100% { transform: rotateX(720deg); }
-}`;
-document.getElementsByTagName('head')[0].appendChild(style);
-
-let animating = false;
-scene.onclick = () => {
-  roller.style.animation = animating ? '' : 'spin 4s infinite';
-  animating = !animating;
-};
-
-
 
 document.body.appendChild(scene);
 document.body.appendChild(controls);
---
+++ js/roller.js
@@ -0,0 +1,143 @@
+/**
+ * ViewBox is a simple container for the 4 vlaues that make up a view box.
+ */
+export class ViewBox {
+    /**
+     * @param {Number} x
+     * @param {Number} y
+     * @param {Number} width
+     * @param {Number} height
+     */
+    constructor(x, y, width, height) {
+        Object.assign(this, { x, y, width, height });
+    }
+}
+
+/**
+ * SVG serves as a container for an svg url and an associated ViewBox.
+ */
+export class SVG {
+    /**
+     * @param {String} url
+     * @param {ViewBox} viewBox
+     */
+    constructor(url, viewBox) {
+        Object.assign(this, { url, viewBox });
+    }
+}
+
+/**
+ * CreateRoller creates an element that contains a 3d roller of the desired
+ * height and width with the provided svgs stacked on top of each other and
+ * wrapped around the roller. For each image provided there will be segsPerImage
+ * segments.
+ *
+ * The roller is formed from a collection of segments, each segment is rotated
+ * around the X axis by an amount inversely proportional to the total number of
+ * segments, and then each segment is Z translated by height/2 such that the
+ * edges of these segments or are aligned and just touching. The function will
+ * determine the segment height such that the translation required to line up
+ * the edges will be equal height/2.
+ * 
+ * @param {Number} width
+ * @param {Number} height
+ * @param {Array}  svgs - an array of @link{SVG} objects.
+ * @param {Number} segsPerImage
+ * @returns {HTMLElement}
+ */
+export function CreateRoller(width, height, svgs, segsPerImage) {
+
+    let numSegs = svgs.length * segsPerImage;
+    let zTranslate = height / 2;
+
+    // Find the angle between adjacent segments.
+    // Angle inside a regular polygon is (n-2) × PI / n where n is the number of
+    // sides.
+    let betweenSides = (numSegs - 2) * Math.PI / 2 / numSegs;
+
+    // Find seg height, it will be twice the adjacent side of the triangle with
+    // opposite side of length zTranslate and angle betweenSides/2;
+    let segHeight = 2 * zTranslate / Math.tan(betweenSides / 2);
+
+    // Helps reduce gaps in rendering to just bridge the gap between components
+    // but also borks the images a bit when more segs per image are used.
+    // could look into overlapping segments, sounds promising.
+    segHeight = Math.trunc(segHeight) + 1;
+
+    let roller = document.createElement("div");
+
+    // @ts-ignore
+    roller.style = `
+    width: ${width}px;
+    height: ${height}px;
+    position: relative;
+    transform-origin: 50% 50%;
+    transform-style: preserve-3d;
+    margin:auto;`;
+
+    /*
+    The default approach taken by fragment identifiers to mapping the viewbox to
+    the viewport(the img tag) is that the viewbox center will be aligned with the
+    viewport center, and it will be scaled maintaining its aspect ratio such that
+    it fills the viewport as far as it can without extending any edges past the
+    viewport. At that point any edges not at the extent of the viewport will be
+    extended to touch the viewport edges. This means if we have defined a square
+    viewbox that we put in a rectangular viewport we will see a rectangular
+    section of the svg.
+    
+    The original kitty viewbox is (0,0,3000,3000) with quite a lot of empty space
+    around the kitty so we want to cut about 500 off each edge. We also want to
+    ensure that each image slice fills the viewport fully vertically, this ensures
+    that segments will align. We are not bothered about the horizontal aspect of our
+    viewbox as long as it is centered. By setting the width to be very small and
+    setting the left offset to the center of the image we ensure that the viewport
+    is fully vertically filled and leave it up to the SVG system to extend the
+    viewbox to the horizontal segment edges.
+    */
+
+    for (let i = 0; i < svgs.length; i++) {
+        let s = svgs[i];
+        let imgSegHeight = s.viewBox.height / segsPerImage;
+
+        for (let j = 0; j < segsPerImage; j++) {
+
+            let seg = document.createElement("img");
+            seg.setAttribute("width", String(width));
+            seg.setAttribute("height", String(segHeight));
+
+            let imgOffset = j * imgSegHeight;
+
+            let svgView = s.url +
+                `#svgView(viewBox(${s.viewBox.left}, 
+                              ${s.viewBox.top + imgOffset}, 
+                              ${s.viewBox.width}, 
+                              ${imgSegHeight}))`;
+
+            seg.setAttribute("src", svgView);
+
+            // @ts-ignore
+            seg.style = `
+                position: absolute;
+                background: grey;
+
+                /* ensure transforms happen around the center of the segment */
+                transform-origin: 50% 50%;
+
+                /*
+                position the segment in the center of it's parent, so push
+                it half the height of its parent down and then pull it
+                up by half of its height
+                */
+                top: 50%;
+                margin-top: ${-segHeight / 2}px;
+
+                /* border: 1px solid red;*/`;
+
+            seg.style.transform = `rotateX(-${360 * (i * segsPerImage + j) / numSegs}deg) translateZ(${zTranslate}px)`;
+
+            roller.appendChild(seg);
+        }
+
+    }
+    return roller;
+}
